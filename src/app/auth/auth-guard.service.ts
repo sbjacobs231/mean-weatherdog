a39331d958192325c9e023cfbda28ae004023571
Sky Jacobson <sky.jacobson231@gmail.com>
2017-11-12 14:09:39 -0800

    turned off auth guard

--- src/app/auth/auth-guard.service.ts
+++ src/app/auth/auth-guard.service.ts
@@ -1,4 +1,9 @@
-import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, Router } from '@angular/router';
+import {
+  CanActivate,
+  ActivatedRouteSnapshot,
+  RouterStateSnapshot,
+  Router
+} from '@angular/router';
 import { Injectable } from '@angular/core';
 import { AuthService } from './auth.service';
 

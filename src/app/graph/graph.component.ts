51fe3e32761c717d817b2ab313bcd5f6b874c0bf
Sky Jacobson <sky.jacobson231@gmail.com>
2017-12-02 17:34:21 -0800

    initial working version of d3 chart on hourly page

--- src/app/graph/graph.component.ts
+++ src/app/graph/graph.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import * as d3 from 'd3';
+import { Observable } from 'rxjs/Observable';
+import { Store } from '@ngrx/store';
+
+import * as fromLocation from '../forecast/store/location.reducers';
 
 @Component({
   selector: 'app-graph',
@@ -6,10 +11,134 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./graph.component.css']
 })
 export class GraphComponent implements OnInit {
+  dataStore: Observable<fromLocation.State>;
+  dataGraph: object[];
 
-  constructor() { }
+  constructor(private store: Store<fromLocation.AppState>) {}
 
   ngOnInit() {
+    this.dataStore = this.store.select('weatherLocation');
+    this.dataStore.subscribe(d => {
+      this.dataGraph = d.hourlyArray
+        .map(each => {
+          return {
+            date: each['FCTTIME']['hour'],
+            temp: each['temp']['english'],
+            humidity: each['humidity']
+          };
+        })
+        .slice(0, 13);
+    });
+    this.drawCharts();
   }
 
+  drawCharts() {
+    // set the dimensions and margins of the graph
+    var margin = { top: 20, right: 20, bottom: 30, left: 20 };
+    var width = 960 - margin.left - margin.right;
+    var height = 275 - margin.top - margin.bottom;
+
+    // parse the date / time
+    var parseTime = d3.timeParse('%H');
+
+    // set the ranges
+    var x = d3.scaleTime().range([0, width]);
+    var y = d3.scaleLinear().range([height, 0]);
+
+    // define the line
+    var valueline1 = d3
+      .line()
+      .x(d => {
+        return x(d.date);
+      })
+      .y(d => {
+        return y(d.temp);
+      });
+
+    // define the line
+    var valueline2 = d3
+      .line()
+      .x(d => {
+        return x(d.date);
+      })
+      .y(d => {
+        return y(d.humidity);
+      });
+
+    var svg = d3
+      .select('svg')
+      .attr('width', width + margin.left + margin.right)
+      .attr('height', height + margin.top + margin.bottom)
+      .append('g')
+      .attr('transform', 'translate(' + margin.left + ',' + margin.top + ')');
+
+    this.dataGraph.forEach(d => {
+      d['date'] = parseTime(d['date']);
+      // d['temp'] = +d['temp'];
+      // d['humidity'] = +d['humidity'];
+    });
+
+    // sort years ascending
+    this.dataGraph.sort((a, b) => {
+      return a['date'] - b['date'];
+    });
+
+    // Scale the range of the data
+    x.domain(
+      d3.extent(this.dataGraph, d => {
+        return d.date;
+      })
+    );
+    y.domain([
+      0,
+      d3.max(this.dataGraph, d => {
+        return Math.max(d.temp, d.humidity);
+      })
+    ]);
+
+    // Add the valueline path.
+    svg
+      .append('path')
+      .data([this.dataGraph])
+      .attr('class', 'line')
+      .attr('d', valueline1)
+      .attr('style', 'fill: none; stroke: red; stroke-width: 3px;');
+    svg
+      .append('text')
+      .text('Temp')
+      .attr('x', width)
+      .data(this.dataGraph)
+      .attr('transform', d => {
+        return 'translate(' + x(d.date) + ',' + y(d.temp) + ')';
+      });
+
+    // Add the valueline path.
+    svg
+      .append('path')
+      .data([this.dataGraph])
+      .attr('class', 'line')
+      .attr('d', valueline2)
+      .attr('style', 'fill: none; stroke: orange; stroke-width: 3px;');
+    svg
+      .append('text')
+      .text('Humidity')
+      .attr('x', width)
+      .data(this.dataGraph)
+      .attr('transform', d => {
+        return 'translate(' + x(d.date) + ',' + y(d.humidity) + ')';
+      });
+    // Add the X Axis
+    svg
+      .append('g')
+      .attr('transform', 'translate(0,' + height + ')')
+      .call(d3.axisBottom(x));
+
+    // Add the Y Axis
+    svg.append('g').call(d3.axisLeft(y));
+
+    d3
+      .selectAll('text')
+      .style('font-size', 14)
+      .style('color', '#555555');
+  }
 }

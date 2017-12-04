8422abd71d01aacc586e7bc180ebf171ecc5e449
Sky Jacobson <sky.jacobson231@gmail.com>
2017-12-03 23:22:30 -0800

    added daily graph component

---
+++ src/app/daily-graph/daily-graph.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-daily-graph',
+  templateUrl: './daily-graph.component.html',
+  styleUrls: ['./daily-graph.component.css']
+})
+export class DailyGraphComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
